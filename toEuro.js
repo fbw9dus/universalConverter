f41ed0dc20d87bcd8d28d7b0de94b8c3be7a6024
Maxim <werkhowski@gmail.com>
2019-09-05 16:18:48 +0200

    Create toEuro.js

---
+++ toEuro.js
@@ -0,0 +1,15 @@
+/* convert from multiple currencies to euro */
+
+/* Params */
+// amount:Number
+// currency:String:3:Uppercase
+
+/* Currencies */
+// Dollar "USD"
+// Yuan ""
+// Rupees ""
+// Kronen(SW)
+// Franken(CH) "CHF"
+
+/* Return */
+// amount:Number (Zwei Kommastellen)
