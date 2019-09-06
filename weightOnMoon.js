4c5e2c9d3046d53809505ecea25b0df57f834a33
botros <botros.nawakil@outlook.de>
2019-09-06 14:14:04 +0200

    done

--- weightOnMoon.js
+++ weightOnMoon.js
@@ -5,3 +5,20 @@
 
 /* Return */
 // kg:Number
+
+
+
+// shcwerkraft in erde = 9.8 
+
+// shcwerkraft in moon = 1.6
+
+// 9.8 / 1.6 = 6.125
+
+
+
+
+function fun (weightOnErth){
+
+    var weightOnMoon = weightOnErth / 6.125 
+   console.log(weightOnMoon)
+   }
