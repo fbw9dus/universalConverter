86f04be7000b44960c7ae0fa917c15cafff56680
Tomislav <tdumancic10@gmail.com>
2019-09-06 10:42:32 +0200

    currency

--- toEuro.js
+++ toEuro.js
@@ -10,6 +10,53 @@
 // Rupees ""
 // Kronen(SW)
 // Franken(CH) "CHF"
+// Croatian Kuna
 
 /* Return */
 // amount:Number (Zwei Kommastellen)
+
+
+var currencieRates = [0.93, 0.13, 0.02, 0.09, 0.92, 0.13]
+
+var usdToEur = currencieRates[0]
+var cnyToEur = currencieRates[1]
+var inrToEur = currencieRates[2]
+var sekToEur = currencieRates[3]
+var chfToEur = currencieRates[4]
+var hrkToEur = currencieRates[5]
+
+var result = 0
+
+function convert(amount, convertTo){
+    if(convertTo === "USD"){
+        result = amount * usdToEur
+    } else if(convertTo === "CNY"){
+        result = amount * cnyToEur
+    } else if(convertTo === "INR"){
+        result = amount * inrToEur
+    } else if(convertTo === "SEK"){
+        result = amount * sekToEur
+    } else if(convertTo === "CHF"){
+        result = amount * chfToEur
+    } else if(convertTo === "HRK"){
+        result = amount * hrkToEur
+    } else if (convertTo === "EUR") {
+        result = amount
+    } else{
+        alert("PLEASE SELECT THE CURRENCY")
+    }
+
+    console.log(amount + " " + convertTo + " = " + result + "EUR")
+   
+}
+
+
+
+
+
+
+
+
+
+
+
